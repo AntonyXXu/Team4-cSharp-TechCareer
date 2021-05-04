6c400c75c0d9b65ebbdc4f31b6be4e25e5130ee9
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-04 15:51:48 -0600

    gitignore issues fixed?

---
+++ TravelExpertsDesktopApp/Travel/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DBModels.Models;
+using System.Configuration;
+
+
+namespace Travel
+{
+    public partial class Form1 : Form
+    {
+
+        TravelExpertsContext context;
+
+        public Form1()
+        {
+            InitializeComponent();
+            context = new TravelExpertsContext();
+            TravelExpertsContext.connectString =
+                ConfigurationManager.ConnectionStrings["TravelExperts"].ConnectionString;
+        }
+
+    }
+}
