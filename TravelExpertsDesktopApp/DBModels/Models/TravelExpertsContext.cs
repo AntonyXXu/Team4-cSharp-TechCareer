c70c17aebae0006f87547833e6c2e59bd3205890
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-04 15:42:32 -0600

    Linking via app.config

--- TravelExpertsDesktopApp/DBModels/Models/TravelExpertsContext.cs
+++ TravelExpertsDesktopApp/DBModels/Models/TravelExpertsContext.cs
@@ -8,6 +8,8 @@ namespace DBModels.Models
 {
     public partial class TravelExpertsContext : DbContext
     {
+        public static string connectString;
+
         public TravelExpertsContext()
         {
         }
@@ -44,8 +46,8 @@ namespace DBModels.Models
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
 
-                //optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["TechSupport"].ConnectionString);
-                optionsBuilder.UseSqlServer("Data Source=LAPTOP-19QS8D7H\\SQLEXPRESS;Initial Catalog=TravelExperts;Integrated Security=True");
+                optionsBuilder.UseSqlServer(connectString);
+                //optionsBuilder.UseSqlServer("Data Source=LAPTOP-19QS8D7H\\SQLEXPRESS;Initial Catalog=TravelExperts;Integrated Security=True");
             }
         }
 
